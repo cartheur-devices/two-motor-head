de5ffabccfa939d28f59fde4101260240107a22e
samuncler <sam@meaning-5.com>
2025-12-14 15:07:02 +0100

    Starting of the codebase

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Cartheur.Animals.Robot;
 
 namespace Head.Controller
 {
     public class Program
     {
+        public static MotorFunctions MotorControl { get; set; }
+        public static bool MotorsInitialized { get; set; }
+        //public UsbCamera Camera { get; set; }
+
         async static Task Main()
         {
+            MotorControl = new MotorFunctions();
+            var value = MotorControl.InitializeDynamixelMotors();
+            MotorsInitialized = MotorFunctions.DynamixelMotorsInitialized;
+            if (MotorsInitialized)
+                MotorControl.CreateConnectMotorObjects();
+
             Console.WriteLine("Hello, World!");
             await Task.CompletedTask;
         }
